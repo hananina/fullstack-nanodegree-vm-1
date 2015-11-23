bb744c4061f6e786deb770ac24d1dfad2179a13f
hananina <bubblesyo86@gmail.com>
2015-11-23 17:34:46 -0500

    add DB.close() to "getPosts" function

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -17,12 +17,14 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    
+
     ## Database connection
     DB = psycopg2.connect("dbname=forum")
 
     posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
     posts.sort(key=lambda row: row['time'], reverse=True)
+
+    DB.close()
     return posts
 
 ## Add a post to the database.
